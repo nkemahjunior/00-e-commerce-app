61e39cb4ef73c61133d703e7111023f36a0cb98a
nkemahjunior <nkemahjunior679205967@gmail.com>
2023-10-03 04:34:54 +0100

    updated home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-const plugin = require('tailwindcss/plugin');
-
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -12,21 +10,5 @@ module.exports = {
 
     },
   },
-  plugins: [
-    plugin(function ({ addVariant, e, postcss }) {
-      addVariant('firefox', ({ container, separator }) => {
-        const isFirefoxRule = postcss.atRule({
-          name: '-moz-document',
-          params: 'url-prefix()',
-        });
-        isFirefoxRule.append(container.nodes);
-        container.append(isFirefoxRule);
-        isFirefoxRule.walkRules((rule) => {
-          rule.selector = `.${e(
-            `firefox${separator}${rule.selector.slice(1)}`
-          )}`;
-        });
-      });
-    }),
-  ],
+  plugins: [],
 }

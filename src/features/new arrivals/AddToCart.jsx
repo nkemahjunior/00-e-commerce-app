93e5e7a2b14e7ab5283c2c21c7c66a25fdb2c187
nkemahjunior <nkemahjunior679205967@gmail.com>
2023-10-11 23:52:50 +0100

    update

--- src/features/new arrivals/AddToCart.jsx
+++ src/features/new arrivals/AddToCart.jsx
@@ -1,7 +1,7 @@
 function AddToCart() {
     return (
         
-            <button className=" bg-yellow-300 md:hover:bg-yellow-400  md:hover:scale-100 scale md:hover:transition-all md:hover:delay-75 rounded-lg capitalize font-bold p-2 text-sm ">
+            <button className=" bg-yellow-300 md:hover:bg-yellow-400  md:hover:scale-95 scale md:hover:transition-all md:hover:delay-75 rounded-lg capitalize font-bold p-2 text-sm ">
                 add to cart
             </button>
         

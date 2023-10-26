df53e2c569ddc7a8d17870c251a001a096d4e7c4
nkemahjunior <nkemahjunior679205967@gmail.com>
2023-10-26 16:36:11 +0100

    update

--- src/features/Admin Duties/AddDresses.jsx
+++ src/features/Admin Duties/AddDresses.jsx
@@ -1,7 +1,26 @@
 function AddDresses() {
     return (
-        <div>
-            
+        <div >
+            <h1 className="uppercase text-2xl font-light">add dresses to new arivals</h1>
+
+            <form action="">
+
+                <div>
+                    <label htmlFor="name"></label>
+                    <input type="text" className="outline-none border-2 border-solid border-black"/>
+                </div>
+
+                <div>
+                    <label htmlFor="price"></label>
+                    <input type="number" className="outline-none border-2 border-solid border-black"/>
+                </div>
+                
+                <div>
+                    <label htmlFor="price"></label>
+                    <input type="number" className="outline-none border-2 border-solid border-black"/>
+                </div>
+
+            </form>
         </div>
     )
 }

90655942e9b848c07b561376eba0de26a04877c0
nkemahjunior <nkemahjunior679205967@gmail.com>
2023-11-19 13:33:17 +0100

    update

---
+++ src/app/sitemap.js
@@ -0,0 +1,40 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://zeco-shopping.vercel.app/",
+      lastModified: new Date(),
+      changeFrequency: "always",
+      priority: 1,
+    },
+    {
+      url: "https://zeco-shopping.vercel.app/newArrivals",
+      lastModified: new Date(),
+      changeFrequency: "always",
+      priority: 0.9,
+    },
+    {
+      url: "https://zeco-shopping.vercel.app/casual",
+      lastModified: new Date(),
+      changeFrequency: "always",
+      priority: 0.9,
+    },
+    {
+      url: "https://zeco-shopping.vercel.app/formal",
+      lastModified: new Date(),
+      changeFrequency: "always",
+      priority: 0.9,
+    },
+    {
+      url: "https://zeco-shopping.vercel.app/party",
+      lastModified: new Date(),
+      changeFrequency: "always",
+      priority: 0.9,
+    },
+    {
+      url: "https://zeco-shopping.vercel.app/gym",
+      lastModified: new Date(),
+      changeFrequency: "always",
+      priority: 0.9,
+    },
+  ];
+}

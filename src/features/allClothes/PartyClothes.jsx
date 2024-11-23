7e40e72cec25129771e0c282d7bd4fd6e872f87f
nkemahJunior <77687679+nkemahjunior@users.noreply.github.com>
2024-11-23 12:58:30 +0100

    update card width

--- src/features/allClothes/PartyClothes.jsx
+++ src/features/allClothes/PartyClothes.jsx
@@ -19,12 +19,12 @@ function PartyClothes() {
   }
 
   return (
-    <div className="mt-4 lg:mt-0   px-2 md:px-[2rem] lg:px-[3rem] xl:px-[4rem] 2xl:px-[6rem] ">
+    <div className="mt-4 lg:mt-0   px-2 md:px-[2rem] lg:px-[3rem] xl:px-[4rem] 2xl:px-[6rem] w-full">
       <h1 className=" mb-2 md:mb-4 capitalize font-bold text-2xl text-center lg:text-3xl xl:text-4xl 2xl:text-5xl lg:mb-6 xl:mb-8 2xl:mb-10">
         party
       </h1>
 
-      <div className="grid grid-cols-2  md:grid-cols-3 xl:grid-cols-4 gap-x-2 gap-y-2">
+      <div className="grid grid-cols-2  md:grid-cols-3 xl:grid-cols-4 gap-x-2 gap-y-2 w-full">
         {data.map((el) => (
           <Suspense fallback={<LoadingCard />} key={el.id}>
             <Link href={`/party/${el.name}/${el.id}`}>
